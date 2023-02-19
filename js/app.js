db6f7f76f368afea570c941aefde622182f43680
Christina Hollie <christinahollie7@gmail.com>
2023-02-18 18:19:26 -0800

    questionOne

--- js/app.js
+++ js/app.js
@@ -9,11 +9,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+let numberSum = a+b 
+    let str = "The sum of "+ a + " and " + b + " is " + numberSum + "."   
+return [numberSum,str]
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+ testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -35,6 +37,7 @@ function multiply(a, b) { //eslint-disable-line
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
+//for problem 3 remember that I only want the 0 index to do the math.
 /////////////////////////////////////
 /* Problem 3
 Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
